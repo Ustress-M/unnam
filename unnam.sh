79a506720f5beca5b915da2f7310940a454a7b61
Ustress-M <38184228+Ustress-M@users.noreply.github.com>
2023-05-09 22:48:43 +0900

    Add files via upload

---
+++ unnam.sh
@@ -0,0 +1,9 @@
+sudo apt autoremove
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt update
+sudo apt upgrade
+sudo apt-get install cheese -y
+sudo apt-get install fonts-unfonts-core ibus-hangul -y
+pip3 install opencv-python opencv-contrib-python
+sudo rpi-update
